06f3176d956068bf4de15430e9d6e2885b67191d
TJ-dotcom <67821626+TJ-dotcom@users.noreply.github.com>
2024-05-16 22:58:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import json
 import requests
+import pandas as pd
 
 st.title('Property Evaluation')
-
 
 with open('input_options.json') as f:
     side_bar_options = json.load(f)
     options = {}
     for key, value in side_bar_options.items():
         if key == 'city':
-            options[key] = st.sidebar.selectbox(key, value)
+            selected_city = st.sidebar.selectbox(key, value)
+            for city in value:
+                options[f'city_{city.replace(" ", "_")}'] = 1 if city == selected_city else 0
         else:
             min_val, max_val = value
             if isinstance(min_val, float) or isinstance(max_val, float):
@@ -21,27 +23,27 @@ with open('input_options.json') as f:
                 current_value = (min_val + max_val) // 2
                 options[key] = st.sidebar.slider(key, min_val, max_val, value=current_value)
 
-
 st.write(options)
 
-
 if st.button('Predict'):
+    # Prepare the input data as a DataFrame
+    input_data = {key: [value] for key, value in options.items()}
+    input_df = pd.DataFrame(input_data)
 
-    payload = json.dumps({'inputs': options})
-    
+    # Convert DataFrame to JSON
+    payload = input_df.to_json(orient='split')
+
     try:
-
         response = requests.post(
             url="http://64.225.10.251:5001/invocations",
             data=payload,
             headers={"Content-Type": "application/json"},
         )
-        
+
         if response.status_code == 200:
             try:
                 # Parse the JSON response
                 prediction = response.json().get('predictions')[0]
-
                 st.write(f'The predicted median house value is: ${prediction:,}')
             except json.JSONDecodeError:
                 st.error("Error decoding the JSON response from the API.")
@@ -49,6 +51,5 @@ if st.button('Predict'):
         else:
             st.error(f"API request failed with status code {response.status_code}.")
             st.write("Response content:", response.text)
-    
     except requests.RequestException as e:
         st.error(f"An error occurred while making the API request: {e}")
