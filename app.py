a4f4544c2dde4fd179277af8b798c7817352bf18
TJ-dotcom <67821626+TJ-dotcom@users.noreply.github.com>
2024-05-16 22:26:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if st.button('Predict'):
     try:
 
         response = requests.post(
-            url="64.225.10.251:5001/invocations",
+            url="http://64.225.10.251:5001/invocations",
             data=payload,
             headers={"Content-Type": "application/json"},
         )
